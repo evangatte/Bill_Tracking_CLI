dcb2979b3a80c02bf2bc7d1560086a01e7dd9b84
evangatte <106684791+evangatte@users.noreply.github.com>
2023-05-17 17:14:16 -0500

    update dist

--- dist/helperFunctions/moneyManagement/spendingAmount.js
+++
@@ -1,6 +0,0 @@
-let currentYear = new Date().getFullYear();
-let currentMonth = new Date().getMonth() + 1;
-let currentDayNum = new Date().getDay();
-let numberOfDaysInMonth = new Date(currentYear, currentMonth, 0).getDate();
-export {};
-//# sourceMappingURL=spendingAmount.js.map
--- dist/helperFunctions/setup.js
+++
@@ -1,6 +0,0 @@
-import { fileURLToPath } from 'url';
-export async function setupBillPath() {
-    const __filename = fileURLToPath(import.meta.url);
-    console.log(__filename);
-}
-//# sourceMappingURL=setup.js.map
